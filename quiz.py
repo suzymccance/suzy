5c5e1a4f91e428271cc242d82b083606c311bf68
suzymccance <smccancelol@gmail.com>
2025-06-24 17:05:31 +0000

    6/24 calculator

--- quiz.py
+++ quiz.py
@@ -2,20 +2,20 @@ e=6
 x=7
 q="-"
 a=-1
-number1=input("what is the first number to the equation?")
+number1=int(input("what is the first number to the equation?"))
 operation=input("what is the operation to the equation")
-number2=input("what is the second number to the equation?")
+number2=int(input("what is the second number to the equation?"))
 equals=input("type the equal sign")
-answer=input("type answer here")
+answer=int(input("type answer here"))
 
 if(number1==e):
     print(e,"✔️")
-
+elif(number1==x):
+    print(x,"🚫")
+elif(number1==a):
+    print(a,"🚫")
 else:
-    if(number1==x):
-        print("🚫")
-    if(number1==a):
-        print("🚫")
+    print(number1,"⬛")
 
 
 
