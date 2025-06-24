adea66c97eb7951e45c3068e62e4f5a248d2fadd
suzymccance <smccancelol@gmail.com>
2025-06-24 14:00:37 +0000

    6/24 10:00

--- quiz.py
+++ quiz.py
@@ -7,19 +7,42 @@ operation=input("what is the operation to the equation")
 number2=input("what is the second number to the equation?")
 equals=input("type the equal sign")
 answer=input("type answer here")
+
 if(number1==e):
     print(e,"✔️")
 
 else:
-    if(number1==)
+    if(number1==x):
+        print("🚫")
+    if(number1==a):
+        print("🚫")
+
+
 
 
 
 
 if(operation==q):
     print(q,"✔️")
+if(q!=operation):
+    print(q,"⬛")
+
+
+
 if(number2==x):
     print(x,"✔️")
+elif(number2==e):
+    print(e,"🚫")
+
+
+print("=")
+
+
 if(answer==a):
     print(a,"✔️")
-    
+elif(answer==x):
+    print(answer,"🚫")   
+elif(answer==e):
+    print(answer,"🚫")
+else:
+    print(answer,"⬛")
