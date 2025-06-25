02a3ec367d5daa53d3c540109a38990c22e72d98
suzymccance <smccancelol@gmail.com>
2025-06-25 14:00:14 +0000

    6/25 10:00

--- guessinggame.py
+++ guessinggame.py
@@ -1,3 +1,12 @@
-x=93
+import random
+x=random.randint(1,100)
+guess=0
 print("The number is 1-100")
-guess=input("What is your guess?")
+while(guess!=x):
+    guess=int(input("What is your guess?"))
+    if(guess==x):
+        print("correct")
+    if(guess>x):
+        print("guess lower")
+    if(guess<x):
+        print("guess higher")
---
+++ rockpaperscissors.py
@@ -0,0 +1,6 @@
+import random
+print("1 is scissors, 2 is paper, and 3 is rock")
+rps=input("What is your move")
+x=random.randint(1,3)
+spr=""
+if(rps==scissors)
