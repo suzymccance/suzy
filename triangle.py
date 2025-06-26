02b00b6ab5a020814a92137f9863cde46d19b94b
suzymccance <smccancelol@gmail.com>
2025-06-26 16:58:06 +0000

    shopping list

--- triangle.py
+++ triangle.py
@@ -1,4 +1,9 @@
-for i in range(100):
+for i in range(10000000000000):
     for j in range(i+1):
         print("▫️",end="")
     print()
+can=int(input("canu give a number to make the square"))
+v=input("what symbole do u want to use for ur square?")
+for i in range(can):
+    print(v,end="")
+print()
